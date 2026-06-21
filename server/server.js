6c541b6469c61d795b6bee498133834204af0367
RavinduUni <fc115680@foc.sjp.ac.lk>
2026-06-21 11:43:02 +0530

    'config-database'

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mainRouter from './routes/mainRouter.js';
 import dotenv from 'dotenv';
+import { connectDB } from './configs/db.js';
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+await connectDB();
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
