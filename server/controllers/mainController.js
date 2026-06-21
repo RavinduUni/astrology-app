d8c0e37dcec998f81e3568d72f2ed8aa50633569
RavinduUni <fc115680@foc.sjp.ac.lk>
2026-06-21 13:19:39 +0530

    'fix-login'

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -46,17 +46,13 @@ export const login = async (req, res) => {
 
     console.log("Login request:", req.body);
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select('+password');
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    console.log("User found:", user);
-
     const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    console.log("Password valid:", isPasswordValid);
 
     if (!isPasswordValid) {
       return res.status(401).json({ message: "Invalid password" });
@@ -64,11 +60,7 @@ export const login = async (req, res) => {
 
     const token = generateToken(user._id);
 
-    console.log("Token generated:", token);
-
     const { password: _, ...userWithoutPassword } = user.toObject();
-
-    console.log("Login successful:", userWithoutPassword);
     
     return res.status(200).json({ message: "Login successful", user: userWithoutPassword, token });
 
